ca8759f48e7c13e17a2cf6ad939f6f596b3eac98
Leonardo da Rosa Dornelles <105314378+LeonardoDaRosaDornelles@users.noreply.github.com>
2023-05-15 19:19:10 -0300

    pagina fializada

--- ajuste.js
+++ ajuste.js
@@ -23,13 +23,56 @@ $('.container-slider').slick({
 function clicou(){
     let burguer = document.querySelector('#burguer')
     let itens = document.querySelector('#itens')
-    if(itens.style.display == 'block'){
-        burguer.style.left = '75vw'
-        itens.style.display = 'none'
-        
+    if(itens.style.display == 'block'){ 
+        burguer.style.background = 'black'
+        burguer.style.color = '#F7CA0F'
+        burguer.style.boxShadow = 'none'
+        burguer.style.right = '0'
+        itens.style.display = 'none'        
     }
     else{
         itens.style.display = 'block'
-        burguer.style.left = '42vw'
+        burguer.style.right = '200px'
+        burguer.style.background = '#F7CA0F'
+        burguer.style.color = 'black'
+        burguer.style.boxShadow = ' 0 0 15px #F7CA0F, inset 0 0 15px #F7CA0F'
+    }
+}
+
+//BOTÕES
+//sobre
+const botaoSobre = document.querySelector('#sobre');
+const sectionSobre = document.querySelector('.sobre');
+
+botaoSobre.addEventListener('click' , () =>{
+    sectionSobre.scrollIntoView({behavior:'smooth'});
+})
+
+//horario
+const botaoHora = document.querySelector('#hora');
+const sectionHora = document.querySelector('.horarios');
+
+botaoHora.addEventListener('click' , () =>{
+    sectionHora.scrollIntoView({behavior:'smooth'});
+})
+
+//depoimentos
+const botaoHate = document.querySelector('#hate');
+const sectionHate = document.querySelector('.depoimentos');
+
+botaoHate.addEventListener('click' , () =>{
+    sectionHate.scrollIntoView({behavior:'smooth'});
+})
+
+//TELA
+function mudouTamanho(){
+    let banner = document.querySelector('#cartaz')
+    if(window.innerWidth > 768){
+        banner.setAttribute('src','imagens/Banner_Neon_Gym.png')
+        banner.Width = ('100vw')
+    }
+    else{
+        banner.setAttribute('src','imagens/Cartaz_Neon_Gym.png')
+        banner.Width = ('100vw')
     }
 }
